f40576abffbe0e551c3c2f331857a4c6cc405151
Gaku <rxf10242@gmail.com>
2022-10-03 23:11:24 +0900

    22.10.3 v.1.42 MZ-700/1500版を追加

--- make.js
+++ make.js
@@ -1,8 +1,8 @@
-// LD.ASZ の INCLUDE を まとめて ZASM/LD.ASM に保存する WSHスクリプト
+// X1.ASZ の INCLUDE を まとめて ZASM/X1.ASM に保存する WSHスクリプト
 
 var fso = WScript.CreateObject("Scripting.FileSystemObject");
-var sASZ = ReadFile("LD.ASZ").replace(/\s*INCLUDE\s+"([^"]+)"/g, IncludeFile);
-var fh = fso.OpenTextFile("ZASM\\LD.ASM", 2, true, 0);
+var sASZ = ReadFile("X1.ASZ").replace(/\s*INCLUDE\s+"([^"]+)"/g, IncludeFile);
+var fh = fso.OpenTextFile("ZASM\\X1.ASM", 2, true, 0);
 fh.Write(sASZ);
 fh.Close();
 
